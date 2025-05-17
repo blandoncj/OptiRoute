95a8f08b61085861f9c522ed4a34209f3764890e
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-05-16 22:56:46 -0500

    feat(app): initialize React app with basic structure
    - Added `App.tsx` with `Header`, `About`, and `Requirements` components.
    - Set up `main.tsx` to render the app using React's `StrictMode`.
    - Included `vite-env.d.ts` for Vite environment type definitions.
    - Added assets: `bus.png` and `vite.svg` to the `public` folder.
    - Imported global styles via `index.css`.
    
    This commit sets up the foundation for the React application.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { About, Header, Requirements } from './components'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Requirements />
+    </>
+  )
+}
+
+export default App
